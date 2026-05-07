ae5408e029114696e4e039ebda4a33f5f0854b36
chaitanyapop <parandkarchaitanya@gmail.com>
2026-05-07 11:28:04 +0530

    completed with basic understanding of apis

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 books = [
-    {'title':"alchemist",'author':'Paul', 'pages':170}
+    {'title':"alchemist",'author':'Paul', 'pages':170},
+    {'title':"atomic habits",'author':'Cnp', 'pages':170}
 ]
+
+class Book(BaseModel):
+    title:str
+    author:str
+    page:int
 
 @app.get("/home")
 def getHomeValue():
@@ -13,3 +20,27 @@ def getHomeValue():
 @app.get("/allBooks")
 def getAllBooks():
     return books
+
+@app.get("/book/{title}/")
+def getBookAuthor(title, author):
+    for book in books:
+        if book.get('title') == title and book.get('author') == author:
+            return book
+        
+@app.post("/book/addBook")
+def addBook(book:Book):
+    books.append(book)
+    return {
+        "message":"Book created",
+        "data": books
+    }
+
+@app.put('/book/updateBook')
+def updateBook(book:Book):
+    # here we can write update function
+    return
+
+@app.delete('/book/deleteBook')
+def updateBook(book:Book):
+    # here we can write delete function
+    return
