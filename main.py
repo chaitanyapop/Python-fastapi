ffb01a9b4698d60c670ba8749656bedcb18f2419
chaitanyapop <parandkarchaitanya@gmail.com>
2026-05-14 12:20:08 +0530

    added model pydantic

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import models as model
 
 app = FastAPI()
 
 books = [
-    {'title':"alchemist",'author':'Paul', 'pages':170},
-    {'title':"atomic habits",'author':'Cnp', 'pages':170}
+    model.Books(id = 1, title = "book 1", author = 'a',description = "book 1 desc",rating = 4),
+    model.Books(id = 2,  title = "book 2", author =  'b',description = "book 2 desc",rating =  3),
+    model.Books(id = 3, title =  "book 3",  author = 'c',description = "book 3 desc",rating =  5),
+    model.Books(id = 4,  title = "book 4",  author = 'd',description = "book 14 desc", rating = 4),
+    model.Books(id = 5,  title = "book 5",  author = 'e',description = "book 5 desc",rating =  2)
 ]
 
-class Book(BaseModel):
-    title:str
-    author:str
-    page:int
+# class Book(BaseModel):
+#     title:str
+#     author:str
+#     page:int
 
 @app.get("/home")
 def getHomeValue():
@@ -19,28 +23,29 @@ def getHomeValue():
 
 @app.get("/allBooks")
 def getAllBooks():
+    print("returning books....")
     return books
 
 @app.get("/book/{title}/")
 def getBookAuthor(title, author):
     for book in books:
-        if book.get('title') == title and book.get('author') == author:
+        if book.title == title and book.author == author:
             return book
         
 @app.post("/book/addBook")
-def addBook(book:Book):
+def addBook(book:model.Books):
     books.append(book)
     return {
         "message":"Book created",
         "data": books
     }
 
-@app.put('/book/updateBook')
-def updateBook(book:Book):
-    # here we can write update function
-    return
+# @app.put('/book/updateBook')
+# def updateBook(book:Book):
+#     # here we can write update function
+#     return
 
-@app.delete('/book/deleteBook')
-def updateBook(book:Book):
-    # here we can write delete function
-    return
+# @app.delete('/book/deleteBook')
+# def updateBook(book:Book):
+#     # here we can write delete function
+#     return
